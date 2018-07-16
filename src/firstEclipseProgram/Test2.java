e3ecd78c5f4aa14eae900cd780c91c03cf99f31a
Harshal <harshal.golatkar@capgemini.com>
2018-07-16 15:38:59 +0530

    Done for version 2

--- src/firstEclipseProgram/Test2.java
+++ src/firstEclipseProgram/Test2.java
@@ -4,7 +4,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is 2nd Test Program");
+		System.out.println("This is 2nd Test Program, some change");
+		System.out.println("This is 2nd Test Program, some change");
 	}
 
 }
