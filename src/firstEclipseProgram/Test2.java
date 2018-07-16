991212079abd0141d5fd6b2c122d2615243a33d4
Harshal <harshal.golatkar@capgemini.com>
2018-07-16 15:37:31 +0530

    This is 2nd file

---
+++ src/firstEclipseProgram/Test2.java
@@ -0,0 +1,10 @@
+package firstEclipseProgram;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is 2nd Test Program");
+	}
+
+}
